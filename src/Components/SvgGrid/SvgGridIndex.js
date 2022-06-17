0ba4fdfd257eb3d17de3dacd864e5f2510a200f2
Mário Xaxá <marioxaxa0@gmail.com>
2022-06-17 04:38:57 -0300

    17/06

--- src/Components/SvgGrid/SvgGridIndex.js
+++ src/Components/SvgGrid/SvgGridIndex.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import SvgGridItemIndex from '../SvgGridItem/SvgGridItemIndex';
 import './SvgGridStyle.css'
 
 export default function SvgGridIndex({ data}) {
   const parser = new DOMParser();
   const [count, setCount] = React.useState('0')
+
+
 
   /*
   Teste
@@ -23,17 +26,13 @@ export default function SvgGridIndex({ data}) {
       {data.map(d => {
         if(!d.filename.startsWith('svg.icon')) {return}
         const doc = parser.parseFromString(d.content, 'text/html');
-        const svg = doc.getElementsByTagName('svg')[0];
+        const currentSvg = doc.getElementsByTagName('svg')[0];
         return(
-          <div className='IconDiv' key={d.filename} onClick={() =>{console.log(d.filename)}} >
-            <div className='IconSvgDiv'>
-              <svg 
-                width={svg.width.baseVal.value}
-                height={svg.height.baseVal.value}
-                dangerouslySetInnerHTML={{__html: svg.innerHTML}}
+              <SvgGridItemIndex
+                key={d.filename}
+                svg={currentSvg}
+                name={d.filename}
               />
-            </div>
-          </div>
         )
       })}
       
---
+++ src/Components/SvgGridItem/SvgGridItemIndex.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import './SvgGridItemStyle.css'
+
+export default function SvgGridItemIndex( {svg, name}) {
+
+    const scaleFunc = () => {
+        let minor
+        if (svg.width.baseVal.value < svg.height.baseVal.value) {
+            minor = svg.width.baseVal.value
+        } else {
+            minor = svg.height.baseVal.value
+        }
+
+        if (minor < 50) {
+            return ('0 0 100 100')
+        } else if (minor < 100) {
+            return ("0 0 150 150")
+        } else if (minor < 200){
+            return ('0 0 250 250')
+        } else if (minor < 300){
+            return ('0 0 350 350')
+        } else if (minor < 400){
+            return ('0 0 450 450')
+        } else {
+            return ('0 0 500 500')
+        }
+    }
+    
+
+
+  return (
+    <div className='ItemDiv' onClick={() => {console.log(name)}}>
+        <div className='SvgDiv'>
+            <svg className='Svg' 
+                viewBox={scaleFunc()}
+                width="100%"
+                height="100%"
+                dangerouslySetInnerHTML={{__html: svg.innerHTML}}
+            />
+        </div>
+        <div className='NameDiv'>
+            <p>
+                {name}
+            </p>
+        </div>
+    </div>
+  )
+}
